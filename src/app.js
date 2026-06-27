140fd3fe384163a18a7855dfc34be688dc74c482
ashu ulape <ashuulape@gmail.com>
2026-06-27 21:56:01 +0530

    all done

--- src/app.js
+++ src/app.js
@@ -8,6 +8,9 @@ const app=express()
 app.use(express.json())
 app.use(cookieparser())
 
+app.get('/',(req,res)=>{
+  res.send('server is working')
+})
 app.use('/api/auth',authroutes)
 app.use('/api/accounts',accountrouter)
 app.use('/api/transaction',transactionRoutes)
